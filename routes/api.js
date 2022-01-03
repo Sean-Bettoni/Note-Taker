432fe7506572956c3d1fd79249ce6dd30e7f085b
Sean <bettoni98@hotmail.com>
2022-01-03 15:46:10 +0800

    Finished routes

--- routes/api.js
+++ routes/api.js
@@ -2,21 +2,24 @@ const express = require('express');
 const path = require('path');
 const router = express.Router();
 const fs = require('fs');
+const { v4: uuidv4 } = require('uuid');
+
+const dbPath = path.join(__dirname, '..', 'db', 'db.json');
 
 
 function getNotes() {
-    const dbPath = path.join(_dirname, '..', 'db', 'db.json');
-    
     // load db.json file
     const file = fs.readFileSync(dbPath, 'utf-8')
     
     // parse the json into js object
     // return it
+    // returning the json library and parsing the file defined above, if not successful it will instead return an empty array
     return JSON.parse(file) || [];
 }
 
 
 
+// get
 router.get('/api/notes', (req, res) => {
     
     // retrieve all of our notes
@@ -24,7 +27,53 @@ router.get('/api/notes', (req, res) => {
 
     res.json(notes);
 
+})
+
+
+
+// post
+router.post('/api/notes', (req, res) => {
+
+    // posting title into title
+    const title = req.body.title;
+    const text = req.body.text;
+
+    // get all existing notes
+    const existingNotes = getNotes();
+
+    // add the new note to the notes array
+    existingNotes.push({
+        id: uuidv4(),
+        title,
+        text,
+    })
+
+    fs.writeFileSync(dbPath, JSON.stringify(existingNotes));
+
+    return res.json(existingNotes);
+})
+
+
+
+
+// delete
+router.delete('/api/notes/:id', (req, res) => {
+    
+    const noteId = req.params.id;
+
+    //get all the notes
+    const notes = getNotes();
+
+    // filter out the note that we dont want
+    const filtered = notes.filter ((note) => note.id !== noteId) 
+
+    // save the remaining notes back into db.json
+    fs.writeFileSync(dbPath, JSON.stringify(filtered));
+
+    // return a response
+    res.json(filtered)
 
 })
+
 
 module.exports = router;
--- server.js
+++ server.js
@@ -9,11 +9,27 @@ const apiRoutes = require('./routes/api');
 const PORT = process.env.PORT || 3001;
 
 // Setting up middleware
+// Middleware is a function that runs before every single request
 app.use(express.static('public'));
+
+// Before the request hits the web/api routes below we are using middleware to understand the json (express.json) and form data(express.urlencoded)
+// This will turn the requests into a workable javascript object
+app.use(express.json());
+
+
+app.use(express.urlencoded({extended: true}));
 
 // Injecting/loading routes into app
 app.use(webRoutes);
 app.use(apiRoutes);
+
+
+
+
+
+// app.get("*", (req, res) => {
+//     res.sendFile(path.join(__dirname, "public", "404.html"));
+// });
 
 
 
